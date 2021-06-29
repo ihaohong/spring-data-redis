7f06718e42bb0b373e6ebc8dc31467b66311953a
ihaohong <haohong725@qq.com>
2021-06-29 22:05:11 +0800

    add ut

--- src/test/java/org/springframework/data/redis/connection/AbstractConnectionIntegrationTests.java
+++ src/test/java/org/springframework/data/redis/connection/AbstractConnectionIntegrationTests.java
@@ -1582,6 +1582,15 @@ public abstract class AbstractConnectionIntegrationTests {
 	}
 
 	@Test
+	void testSMIsMember() {
+		actual.add(connection.sAdd("myset", "foo"));
+		actual.add(connection.sAdd("myset", "bar"));
+		actual.add(connection.sIsMember("myset", "foo", "bar"));
+		actual.add(connection.sIsMember("myset", "foo", "bar2"));
+		verifyResults(Arrays.asList(new Object[] { 1L, 1L, Arrays.asList(true, true), Arrays.asList(true, false) }));
+	}
+
+	@Test
 	void testSMove() {
 		actual.add(connection.sAdd("myset", "foo"));
 		actual.add(connection.sAdd("myset", "bar"));
